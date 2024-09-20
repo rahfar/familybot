a92a664750371402eb7de0d697f48ad143c05528
Farid <rahfar777@gmail.com>
2024-09-20 03:07:19 +0300

    Fix

--- src/bot/bot.go
+++ src/bot/bot.go
@@ -101,7 +101,7 @@ func (b *Bot) mourningDigest() string {
 		EURRUB_yesterday := xr_yesterday.Data.RUB.Value / xr_yesterday.Data.EUR.Value
 		BTCUSD_yesterday := 1.0 / xr_yesterday.Data.BTC.Value
 
-		text += fmt.Sprintf("\n__Курсы валют:__\nUSD %.2f₽ (%+.2f%%) \nEUR %.2f₽ (%+.2f%%)\nBTC %.2f$ (%+.2f%%)\n",
+		text += fmt.Sprintf("\nКурсы валют:\nUSD %.2f₽ (%+.2f%%) \nEUR %.2f₽ (%+.2f%%)\nBTC %.2f$ (%+.2f%%)\n",
 			USDRUB_today,
 			(USDRUB_today/USDRUB_yesterday-1)*100,
 			EURRUB_today,
@@ -117,7 +117,7 @@ func (b *Bot) mourningDigest() string {
 		return weather[i].Current.Temp < weather[j].Current.Temp
 	})
 	if len(weather) > 0 {
-		text += "\n__Прогноз погоды:__\n"
+		text += "\nПрогноз погоды:\n"
 		for _, w := range weather {
 			text += fmt.Sprintf("%s: %+g°C (max: %+g°C, min: %+g°C), %s \n", w.Location.Name, w.Current.Temp, w.Forecast.Forecastday[0].Day.Maxtemp_c, w.Forecast.Forecastday[0].Day.Mintemp_c, w.Current.Condition.Text)
 		}
@@ -128,7 +128,7 @@ func (b *Bot) mourningDigest() string {
 	if (err != nil) || (len(news) == 0) {
 		slog.Error("error calling news api", "err", err)
 	} else {
-		fmt_news := fmt.Sprintf("\n__Последние новости c сайта %s:__\n", b.MinifluxAPI.SiteURL)
+		fmt_news := fmt.Sprintf("\nПоследние новости c сайта %s:\n", b.MinifluxAPI.SiteURL)
 		for i, n := range news {
 			translatedTitle, err := b.DeeplAPI.CallDeeplAPI([]string{n.Title})
 			if err != nil {
